54def796368c3f39a67e518440f525d67e5fb07f
anshuman@simpledatalabs.com <anshuman@simpledatalabs.com>
2022-05-25 15:12:59 +0000

    commit

---
+++ pipelines/data_generator/code/job/graph/__init__.py
@@ -0,0 +1,7 @@
+from .Reformat_1_3 import *
+from .acc_status_1_csv import *
+from .Reformat_1_2 import *
+from .Reformat_1_1 import *
+from .Reformat_1 import *
+from .Reformat_1_4 import *
+from .Generator import *
---
+++ pipelines/data_generator/code/job/pipeline.py
@@ -0,0 +1,31 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from job.config.ConfigStore import *
+from job.udfs.UDFs import *
+from job.graph import *
+
+def pipeline(spark: SparkSession) -> None:
+    (df_Generator_acc_status_first_df,  df_Generator_acc_status_second_df,  df_Generator_transactions_df,      df_Generator_people_df,  df_Generator_products_df) = Generator(
+        spark
+    )
+    df_Reformat_1_1 = Reformat_1_1(spark, df_Generator_acc_status_second_df)
+    df_Reformat_1 = Reformat_1(spark, df_Generator_acc_status_first_df)
+    df_Reformat_1_3 = Reformat_1_3(spark, df_Generator_people_df)
+    acc_status_1_csv(spark, df_Generator_acc_status_first_df)
+    df_Reformat_1_4 = Reformat_1_4(spark, df_Generator_products_df)
+    df_Reformat_1_2 = Reformat_1_2(spark, df_Generator_transactions_df)
+
+def main():
+    Utils.initializeFromArgs(Utils.parseArgs())
+    spark = SparkSession.builder\
+                .config("spark.default.parallelism", "4")\
+                .config("spark.sql.legacy.allowUntypedScalaUDF", "true")\
+                .enableHiveSupport()\
+                .appName("Prophecy Pipeline")\
+                .getOrCreate()
+    initialize(spark)
+    pipeline(spark)
+
+if __name__ == "__main__":
+    main()
