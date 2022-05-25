0d9bf53b40480f1d19aa59817ddd9c02dbeca81a
anshuman@simpledatalabs.com <anshuman@simpledatalabs.com>
2022-05-25 15:44:36 +0000

    commit

--- pipelines/data_generator/code/job/graph/__init__.py
+++ pipelines/data_generator/code/job/graph/__init__.py
@@ -1,5 +1,5 @@
 from .acc_status_1_csv import *
 from .trans_parquet import *
-from .acc_20220505_csv_1 import *
 from .Reformat_1_4 import *
 from .Generator import *
+from .acc_status_2_csv import *
