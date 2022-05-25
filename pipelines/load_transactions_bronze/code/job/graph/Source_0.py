a8b0338934546d9b3d4713a34fce57793febce28
anshuman@simpledatalabs.com <anshuman@simpledatalabs.com>
2022-05-25 13:22:30 +0000

    commit

---
+++ pipelines/load_transactions_bronze/code/job/graph/Source_0.py
@@ -0,0 +1,18 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from job.config.ConfigStore import *
+from job.udfs.UDFs import *
+
+def Source_0(spark: SparkSession) -> DataFrame:
+    if Config.fabricName == "recipes_fabric":
+        return spark.read\
+            .format("parquet")\
+            .schema(
+              StructType([
+                StructField("acc_id", IntegerType(), True), StructField("tran_id", StringType(), True), StructField("business_date", DateType(), True), StructField("tran_amount", DoubleType(), True), StructField("tran_type", StringType(), True), StructField("tran_ts", TimestampType(), True)
+            ])
+            )\
+            .load("dbfs:/Prophecy/anshuman@simpledatalabs.com/fin_reporting/external/transactions/transactions_2022-05-05.parquet")
+    else:
+        raise Exception("No valid dataset present to read fabric")
