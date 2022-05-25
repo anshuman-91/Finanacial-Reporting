000577958e655ef035f55b7a8f1e22e2d7bc5ba3
anshuman@simpledatalabs.com <anshuman@simpledatalabs.com>
2022-05-25 15:27:35 +0000

    created acc_status datagen

--- pipelines/data_generator/code/job/graph/acc_20220504_csv.py
+++ pipelines/data_generator/code/job/graph/acc_20220504_csv.py
@@ -4,14 +4,14 @@ from pyspark.sql.types import *
 from job.config.ConfigStore import *
 from job.udfs.UDFs import *
 
-def acc_status_1_csv(spark: SparkSession, in0: DataFrame):
+def acc_20220504_csv(spark: SparkSession, in0: DataFrame):
     if Config.fabricName == "recipes_fabric":
         in0.write\
             .option("header", True)\
             .option("sep", ",")\
-            .mode("ignore")\
+            .mode("overwrite")\
             .option("separator", ",")\
             .option("header", True)\
-            .csv("dbfs:/Prophecy/anshuman@simpledatalabs.com/fin_reporting/external/acc_status/")
+            .csv("dbfs:/Prophecy/anshuman@simpledatalabs.com/fin_reporting/external/acc_status/acc_status_2022-05-04.csv")
     else:
         raise Exception("No valid dataset present to read fabric")
--- pipelines/data_generator/code/job/pipeline.py
+++ pipelines/data_generator/code/job/pipeline.py
@@ -9,10 +9,9 @@ def pipeline(spark: SparkSession) -> None:
     (df_Generator_acc_status_first_df,  df_Generator_acc_status_second_df,  df_Generator_transactions_df,      df_Generator_people_df,  df_Generator_products_df) = Generator(
         spark
     )
-    df_Reformat_1_1 = Reformat_1_1(spark, df_Generator_acc_status_second_df)
-    df_Reformat_1 = Reformat_1(spark, df_Generator_acc_status_first_df)
+    acc_20220505_csv_1(spark, df_Generator_acc_status_second_df)
     df_Reformat_1_3 = Reformat_1_3(spark, df_Generator_people_df)
-    acc_status_1_csv(spark, df_Generator_acc_status_first_df)
+    acc_20220504_csv(spark, df_Generator_acc_status_first_df)
     df_Reformat_1_4 = Reformat_1_4(spark, df_Generator_products_df)
     df_Reformat_1_2 = Reformat_1_2(spark, df_Generator_transactions_df)
 
