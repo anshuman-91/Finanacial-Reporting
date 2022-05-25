23010bf315cb264a7548c8160bbab8af16e2ea8d
anshuman@simpledatalabs.com <anshuman@simpledatalabs.com>
2022-05-25 13:51:57 +0000

    commit

---
+++ pipelines/load_products_bronze/code/job/graph/Source_0.py
@@ -0,0 +1,19 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from job.config.ConfigStore import *
+from job.udfs.UDFs import *
+
+def Source_0(spark: SparkSession) -> DataFrame:
+    if Config.fabricName == "recipes_fabric":
+        return spark.read\
+            .format("avro")\
+            .option("recursiveFileLookup", True)\
+            .schema(
+              StructType([
+                StructField("id", StringType(), True), StructField("name", StringType(), True), StructField("properties", ArrayType(StructType([]), True), True), StructField("slug", StringType(), True), StructField("updated_at", TimestampType(), True)
+            ])
+            )\
+            .load("dbfs:/Prophecy/anshuman@simpledatalabs.com/fin_reporting/external/products")
+    else:
+        raise Exception("No valid dataset present to read fabric")
