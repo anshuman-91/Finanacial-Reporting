b22f146fde0704d026e3a765865c8bcd23f553d1
anshuman@simpledatalabs.com <anshuman@simpledatalabs.com>
2022-05-26 07:02:30 +0000

    commit

--- pipelines/load_transactions_silver/code/job/graph/transactions_silver.py
+++ pipelines/load_transactions_silver/code/job/graph/transactions_silver.py
@@ -7,11 +7,8 @@ from job.udfs.UDFs import *
 def transactions_silver(spark: SparkSession, in0: DataFrame):
     if Config.fabricName == "recipes_fabric":
         in0.write\
-            .option("header", True)\
-            .option("sep", ",")\
-            .mode("error")\
-            .option("separator", ",")\
-            .option("header", True)\
-            .csv("dbfs:/Prophecy/anshuman@simpledatalabs.com/fin_reporting/acc_status/silver")
+            .format("parquet")\
+            .mode("append")\
+            .save("dbfs:/Prophecy/anshuman@simpledatalabs.com/fin_reporting/transactions/silver/")
     else:
         raise Exception("No valid dataset present to read fabric")
