9a9d422bbf9f33adfa491113fc7fc4a9a43c24ca
anshuman@simpledatalabs.com <anshuman@simpledatalabs.com>
2022-05-26 08:46:21 +0000

    account status silver done

--- pipelines/load_transactions_silver/code/job/graph/__init__.py
+++ pipelines/load_transactions_silver/code/job/graph/__init__.py
@@ -1,4 +1,5 @@
 from .reconcile import *
+from .null_check import *
 from .transactions_bronze import *
 from .transactions_silver import *
 from .dedupe import *
---
+++ pipelines/load_transactions_silver/code/job/graph/null_check.py
@@ -0,0 +1,24 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from job.config.ConfigStore import *
+from job.udfs.UDFs import *
+
+def null_check(spark: SparkSession, in0: DataFrame) -> DataFrame:
+
+    if (
+        in0.filter(
+    col("acc_id").isNull()
+    | col("tran_amount").isNull()
+    | col("tran_type").isNull()
+    | col("tran_id").isNull() 
+    | col("business_date").isNull()
+    | col("tran_ts").isNull()
+    ).count()
+        > 0
+    ):
+        raise Exception("Schema Validation Failed")
+
+    out0 = in0
+
+    return out0
