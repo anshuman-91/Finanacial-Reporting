8a0c735474fc0027ea04a7d5fbc33599461b064d
anshuman@simpledatalabs.com <anshuman@simpledatalabs.com>
2022-05-28 14:06:50 +0000

    fixed trans silver

--- pipelines/load_transactions_silver/code/job/graph/reconcile/acc_status_silver.py
+++ pipelines/load_transactions_silver/code/job/graph/reconcile/acc_status_silver.py
@@ -4,13 +4,13 @@ from pyspark.sql.types import *
 from job.config.ConfigStore import *
 from job.udfs.UDFs import *
 
-def balances(spark: SparkSession) -> DataFrame:
+def acc_status_silver(spark: SparkSession) -> DataFrame:
     if Config.fabricName == "recipes_fabric":
         return spark.read\
             .format("parquet")\
             .schema(
               StructType([
-                StructField("acc_id", IntegerType(), True), StructField("person_id", StringType(), True), StructField("product_id", StringType(), True), StructField("business_date", DateType(), True), StructField("balance", DoubleType(), True)
+                StructField("acc_id", IntegerType(), True), StructField("person_id", StringType(), True), StructField("product_id", StringType(), True), StructField("balance", DoubleType(), True), StructField("business_date", DateType(), True), StructField("import_ts", TimestampType(), True)
             ])
             )\
             .load("dbfs:/Prophecy/anshuman@simpledatalabs.com/fin_reporting/acc_status/silver/")
--- pipelines/load_transactions_silver/code/job/graph/reconcile/reconcile.py
+++ pipelines/load_transactions_silver/code/job/graph/reconcile/reconcile.py
@@ -6,9 +6,9 @@ from . import *
 def reconcile(spark: SparkSession, in0: DataFrame) -> DataFrame:
     df_signed_tran_amt_1 = signed_tran_amt_1(spark, in0)
     df_sum_trans = sum_trans(spark, df_signed_tran_amt_1)
-    df_balances = balances(spark)
-    df_add_row_num2 = add_row_num2(spark, df_balances)
-    df_row_num_eq1 = row_num_eq1(spark, df_add_row_num2)
+    df_acc_status_silver = acc_status_silver(spark)
+    df_add_row_num = add_row_num(spark, df_acc_status_silver)
+    df_row_num_eq1 = row_num_eq1(spark, df_add_row_num)
     df_prev_day_balance = prev_day_balance(spark, df_row_num_eq1)
     df_balance_change = balance_change(spark, df_prev_day_balance)
     df_join_tran_balance = join_tran_balance(spark, df_sum_trans, df_balance_change)
